d1e5ad8327b286fa331dde13874981f29c184ffd
Maboxy <paul@hackenberg.info>
2021-03-20 20:03:23 +0100

    Initial commit

---
+++ Info II - Vorlesung/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Info II - Vorlesung
+//
+//  Created by Paul Hackenberg on 20.03.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
